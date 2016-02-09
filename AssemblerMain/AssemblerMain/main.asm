3084397b5609248a514d37fffea9208e5d873da8
jimmi andersen <joa@jimmiandersen.dk>
2016-02-09 11:13:48 +0100

    main working

--- AssemblerMain/AssemblerMain/main.asm
+++ AssemblerMain/AssemblerMain/main.asm
@@ -6,7 +6,14 @@
 ;
 
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+	   LDI    R16, 0xFF		;load 255 into R16
+       OUT    DDRB, R16		;make port B an output port
+	   OUT    PORTB, R16 	;clear leds
+
+Loop:
+		LDI R18, 0x0		;load 0 into R18
+		OUT DDRA, R18		;make port A an input port
+		IN  R19, PINA		;load the contens of port A (the switches) into R19
+		OUT PORTB, R19		;send the contens of R19 to port B (the leds)     
+RJMP Loop				    ; Jump back to Loop
+   
